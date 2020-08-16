cd4d6eb5963e0fbe46ca4ee5c4bd4b39b3ab85a1
Utku Demir <utkdmr1@gmail.com>
2020-08-16 18:00:20 +0300

    first commit

---
+++ ios/Pods/Target Support Files/Pods-api_example-api_exampleTests/Pods-api_example-api_exampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_api_example_api_exampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_api_example_api_exampleTests
+@end
---
+++ src/Router.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/Auth/Login'
+import Register from './screens/Auth/Register'
+import Home from './screens/Home/Home'
+import AddItem from './screens/Home/AddItem'
+
+
+import { navigationRef } from './RootNavigation';
+import AsyncStorage from '@react-native-community/async-storage';
+import { LOCAL_AUTH_ID, USER } from './actions/types';
+
+const Stack = createStackNavigator();
+
+function Router(props) {
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator initialRouteName='Login'>
+
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={({ navigation, route }) => ({
+                        title: 'Login',
+                    })}
+                />
+
+
+                <Stack.Screen
+                    name="Register"
+                    component={Register}
+                    options={{ title: 'Register' }}
+                />
+
+
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={({ navigation, route }) => ({
+                        title: 'Home',
+                        headerLeft: () => (
+                            <TouchableOpacity
+                                onPress={() => {
+                                    AsyncStorage.removeItem(LOCAL_AUTH_ID)
+                                    USER.token = null
+                                    navigation.replace('Login')
+                                }}
+                                style={{
+                                    marginRight: 20
+                                }}
+                            >
+                                <Image source={require('./img/logout.png')} style={{ width: 20, height: 20, margin: 10 }} />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity
+                                onPress={() => navigation.navigate('AddItems')}
+                                style={{
+                                    marginRight: 20
+                                }}
+                            >
+                                <Text style={{ fontSize: 30 }}>+</Text>
+                            </TouchableOpacity>
+                        ),
+                    })}
+
+                />
+
+                <Stack.Screen
+                    name="AddItems"
+                    component={AddItem}
+                    options={{ title: 'Add Characters' }}
+                />
+
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Router;
